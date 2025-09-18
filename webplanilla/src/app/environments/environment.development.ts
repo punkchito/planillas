14ca46ca1546f0805ede28ef1adcc2cc4a55a771
DESARROLLADOR286_USI <46493780+joinitierlpucallpa@users.noreply.github.com>
2025-09-18 14:46:32 -0500

    fix #9 ajuste enviroment

--- webplanilla/src/app/environments/environment.development.ts
+++ webplanilla/src/app/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api' // URL local para desarrollo
+  apiUrl: 'http://34.176.222.150:3000/api' // URL local para desarrollo
 };
